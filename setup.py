60700f7f3997de0b1b0468a656f6501cfca97cdd
Ramky <uramakrishna21@gmail.com>
2023-06-09 20:59:23 +0200

    Third Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+name='Project_one',
+version = '0.0.1',
+author='Ramky',
+author_email='uramakrishna21@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
